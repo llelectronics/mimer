94e4e3eb1b9a623aeb8fda4f2f17fb910c2a3527
Ancelad <i_am_pomidorchik@mail.ru>
2016-11-03 01:36:38 +0500

    Russian translation

---
+++ translations/harbour-mimer-ru.ts
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A simple app to set default applications.</source>
+        <translation>Простое приложение для настройки приложений по умолчанию.</translation>
+    </message>
+</context>
+<context>
+    <name>AppsList</name>
+    <message>
+        <source>Hide search field</source>
+        <translation>Скрыть поле поиска</translation>
+    </message>
+    <message>
+        <source>Show search field</source>
+        <translation>Показать поле поиска</translation>
+    </message>
+    <message>
+        <source>Select App</source>
+        <translation>Выберите приложение</translation>
+    </message>
+    <message>
+        <source>Reset / Set default</source>
+        <translation>Сброс / Установить умолчания</translation>
+    </message>
+    <message>
+        <source>Hide hidden Applications</source>
+        <translation>Скрыть скрытые приложения</translation>
+    </message>
+    <message>
+        <source>Show hidden Applications</source>
+        <translation>Показать скрытые приложения</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Mimer</source>
+        <translation>Mimer</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Set default apps</source>
+        <translation>Настройте приложения</translation>
+    </message>
+    <message>
+        <source>Browser</source>
+        <translation>Браузер</translation>
+    </message>
+    <message>
+        <source>Change</source>
+        <translation>Изменить</translation>
+    </message>
+    <message>
+        <source>Image Viewer</source>
+        <translation>Изображения</translation>
+    </message>
+    <message>
+        <source>Music Player</source>
+        <translation>Муз. плеер</translation>
+    </message>
+    <message>
+        <source>Video Player</source>
+        <translation>Видеоплеер</translation>
+    </message>
+    <message>
+        <source>Set everything to defaults</source>
+        <translation>Сброс всех настроек</translation>
+    </message>
+    <message>
+        <source>Manual edit</source>
+        <translation>Ручное редактирование</translation>
+    </message>
+    <message>
+        <source>Opening...</source>
+        <translation>Открытие...</translation>
+    </message>
+    <message>
+        <source>Text Editor</source>
+        <translation>Редактор текста</translation>
+    </message>
+</context>
+</TS>
